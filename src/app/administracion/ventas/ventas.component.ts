94cc7d1c4d901ab6e4aac4d98a907a0b27319b59
santiago antonio <santiagoantoniomariscal@gmail.com>
2020-09-10 00:38:46 -0500

    terminar de empezar

--- src/app/administracion/ventas/ventas.component.ts
+++ src/app/administracion/ventas/ventas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UsuariosService } from '../../providers/usuarios.service';
+import { ServiciosService } from '../../providers/servicios.service';
 
 declare var $;
 declare var alertify: any;
@@ -10,43 +11,57 @@ declare var alertify: any;
   styleUrls: ['./ventas.component.css']
 })
 export class VentasComponent implements OnInit {
-  @ViewChild("nombrecliente") nombreClienteId:ElementRef;
-  @ViewChild("apellidocliente") apellidoClienteId:ElementRef;
-  @ViewChild("objValorService") objValorService:ElementRef;
-  @ViewChild("objDateEntrega") objDateEntrega:ElementRef;
-  @ViewChild("objTimeEntrega") objTimeEntrega:ElementRef;
-  @ViewChild("checkUser") checkUser:ElementRef;
-  @ViewChild("id_cliente") id_cliente:ElementRef;
-public usuarios;
-public clientebool:boolean = false;
-public valorservicio:boolean = false;
-public arregloProductos:any = [];
-public venta = {
-  generarServicio:undefined,
-  idcliente:undefined,
-  fecha:undefined,
-  tipoServicio:undefined,
-  idsucursal:undefined,
-  fechaentrega:undefined,
-  hora:undefined,
-  idempleado:undefined
-}
-public conventa:any = {
-  conventa:false
-};
-  constructor(public usuariosPrd:UsuariosService) { }
+  @ViewChild("nombrecliente") nombreClienteId: ElementRef;
+  @ViewChild("apellidocliente") apellidoClienteId: ElementRef;
+  @ViewChild("objValorService") objValorService: ElementRef;
+  @ViewChild("objDateEntrega") objDateEntrega: ElementRef;
+  @ViewChild("objTimeEntrega") objTimeEntrega: ElementRef;
+  @ViewChild("checkUser") checkUser: ElementRef;
+  @ViewChild("id_cliente") id_cliente: ElementRef;
+
+  public usuarios;
+  public clientebool: boolean = false;
+  public valorservicio: boolean = false;
+  public arregloProductos: any = [];
+  public desabilitar:boolean = false;
+  public indexSeleccionado;
+  public venta = {
+    generarServicio: undefined,
+    idcliente: undefined,
+    fecha: new Date().toLocaleDateString(),
+    tipoServicio: undefined,
+    idsucursal: undefined,
+    fechaentrega: undefined,
+    hora: undefined,
+    idempleado: undefined,
+    idservicio: 0
+
+  }
+
+  public arregloVentas:any = [];
+  public conventa: any = {
+    conventa: false
+  };
+  constructor(public usuariosPrd: UsuariosService, private serviciosPrd: ServiciosService) { }
 
   ngOnInit() {
     this.usuarios = this.usuariosPrd.getUsuario();
-    console.log(this.usuarios.usuario);
+    this.serviciosPrd.getVentas(false).subscribe(datos =>{
+      console.log("Esto es lpo que trae2");
+      console.log(datos);
+      this.arregloVentas = datos;
+      if(this.arregloVentas.length > 1){
+       $('#myModalVentas').modal('show');
+      }
+    });
   }
 
-  public abriClientes(){
+  public abriClientes() {
     $('#myModalclientees').modal('show');
 
   }
 
-  public recibir($evento){
+  public recibir($evento) {
     $('#myModalclientees').modal('hide');
     this.nombreClienteId.nativeElement.value = $evento.nombre;
     this.apellidoClienteId.nativeElement.value = $evento.apellido;
@@ -54,17 +69,17 @@ public conventa:any = {
   }
 
 
-  public abrir(){
+  public abrir() {
     $('#myModalProductos').modal('show');
   }
 
-  public recibirProducto($evento){
+  public recibirProducto($evento) {
     $('#myModalProductos').modal('hide');
     this.arregloProductos.push($evento);
   }
 
 
-  public generarServicio(){
+  public generarServicio() {
     //alertify.error('Error, falta datos que rellenar');
     let conventa1 = this.conventa;
     let venta = this.venta;
@@ -74,47 +89,76 @@ public conventa:any = {
     let usuarios = this.usuarios;
     let checkUser = this.checkUser;
     let id_cliente = this.id_cliente;
+    let serviciosPrd = this.serviciosPrd;
+
     alertify.confirm("¿Desea generar un nuevo servicio?", function (e) {
       if (e) {
 
-              if(objValorService.nativeElement.value == "false"){
-         
-           if(objDateEntrega.nativeElement.value == ""){
-            alertify.error("Servicio de entrega no se a asignado fecha de entrega");
-            return;
-           }else if(objTimeEntrega.nativeElement.value == ""){
-            alertify.error("Servicio de entrega no se a asignado hora de entrega");
-            return;
-           }
-        }
-         
-      
+        if (objValorService.nativeElement.value == "false") {
 
-        if(checkUser.nativeElement.checked){
-          if(id_cliente.nativeElement.value == ""){            
+          if (objDateEntrega.nativeElement.value == "") {
+            alertify.error("No se ha asignado fecha de entrega para servicio por encargo");
+            return;
+          } else if (objTimeEntrega.nativeElement.value == "") {
+            alertify.error("No se ha asignado hora de entrega para servicio por encargo");
+            return;
+          }
+        }
+
+
+
+        if (checkUser.nativeElement.checked) {
+          if (id_cliente.nativeElement.value == "") {
             alertify.error("No se ha seleccionado el cliente");
             return;
           }
         }
 
-        conventa1.conventa = true;
-        alertify.success("El servicio esta generado, favor de ingresar los produtos correspondientes y cerrar el ticket");
-        venta.fecha = new Date();
-        if(objDateEntrega != undefined){
+        venta.fecha = new Date().toLocaleDateString();
+        if (objDateEntrega != undefined) {
           venta.fechaentrega = objDateEntrega.nativeElement.value;
         }
-        if(objTimeEntrega != undefined){
+        if (objTimeEntrega != undefined) {
           venta.hora = objTimeEntrega.nativeElement.value;
         }
-        venta.tipoServicio = objValorService.nativeElement.value ? 1 : 2;
+
+        if (id_cliente != undefined) {
+          venta.idcliente = id_cliente.nativeElement.value;
+        }
+
+        venta.tipoServicio = objValorService.nativeElement.value == "true" ? 1 : 2;
         venta.idempleado = usuarios.id;
-        
+
+        serviciosPrd.realizarVenta(venta).subscribe(datos => {
+          venta.idservicio = datos.idservicio;
+          alertify.success("El servicio esta generado, favor de ingresar los produtos correspondientes y cerrar el ticket");
+          conventa1.conventa = true;
+        }, err => {
+          alertify.error("Error al generar el ticket de compra");
+        });
+
 
       }
     });
-  
+
   }
 
-  
+  public seleccionar(item,indice){
+    for(let aux of this.arregloVentas)
+    aux.seleccionado = false;
+
+this.arregloVentas[indice].seleccionado = true;
+    this.desabilitar = false;
+    this.indexSeleccionado = indice;
+  }
+
+
+  public seleccionarItem(){
+    let item = this.arregloVentas[this.indexSeleccionado];
+    console.log(item);
+    $('#myModalVentas').modal('hide');
+  }
+
+
 
 }
--- src/app/providers/servicios.service.ts
+++ src/app/providers/servicios.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { direcciones } from '../direcciones';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
+  public url: string = "";
+  constructor(public http: HttpClient) {
+    this.url = direcciones.ventas;
+  }
 
-  constructor() { }
+  public realizarVenta(obj):Observable<any>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    let json = JSON.stringify(obj);
+    return this.http.post(this.url,json,httpOptions);
+  }
+
+  public getVentas(cobrado:boolean):Observable<any>{
+   return this.http.get(`${this.url}?cobrado=${cobrado}`);
+  }
 }
